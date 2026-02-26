361b2c307d4c191690450c3f5a22629dbcaa9d40
Daniel Lupu <daniel.lupu@1und1.de>
2026-02-26 20:31:47 +0100

    Fix Spline viewer full-screen layout and update to latest viewer version

--- components/sections/SplineScene.tsx
+++ components/sections/SplineScene.tsx
@@ -13,18 +13,16 @@ declare global {
 
 export function SplineScene() {
     return (
-        <div className="absolute inset-0 z-0 w-full h-full overflow-hidden bg-black flex items-center justify-center">
+        <div className="absolute inset-0 z-0 w-full h-full overflow-hidden bg-black">
             <Script
                 type="module"
-                src="https://unpkg.com/@splinetool/viewer@1.12.58/build/spline-viewer.js"
+                src="https://unpkg.com/@splinetool/viewer@1.12.61/build/spline-viewer.js"
                 strategy="afterInteractive"
             />
-            {/* Desktop: takes up full width/height. Mobile: fixed large size scaled down to fit */}
-            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[1200px] h-[1200px] scale-[0.32] min-[370px]:scale-[0.35] min-[400px]:scale-[0.4] sm:scale-[0.5] md:w-full md:h-[calc(100%+50px)] md:scale-100 md:left-0 md:top-0 md:transform-none md:pb-[50px]">
-                {/* Spline Animation: https://prod.spline.design/0gxWuilLejuN3CoM/scene.splinecode */}
+            <div className="w-full h-full">
                 <spline-viewer
                     url="https://prod.spline.design/0gxWuilLejuN3CoM/scene.splinecode"
-                    className="w-full h-full object-cover"
+                    className="w-full h-full"
                 />
             </div>
         </div>
