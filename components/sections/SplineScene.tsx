646494d7d4df210fbbabc6536c3c49ed6615677e
Daniel Lupu <daniel.lupu@1und1.de>
2026-02-26 20:15:00 +0100

    Update Spline animation scene URL and add documentation comment

--- components/sections/SplineScene.tsx
+++ components/sections/SplineScene.tsx
@@ -21,6 +21,7 @@ export function SplineScene() {
             />
             {/* Desktop: takes up full width/height. Mobile: fixed large size scaled down to fit */}
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[1200px] h-[1200px] scale-[0.32] min-[370px]:scale-[0.35] min-[400px]:scale-[0.4] sm:scale-[0.5] md:w-full md:h-[calc(100%+50px)] md:scale-100 md:left-0 md:top-0 md:transform-none md:pb-[50px]">
+                {/* Spline Animation: https://prod.spline.design/0gxWuilLejuN3CoM/scene.splinecode */}
                 <spline-viewer
                     url="https://prod.spline.design/0gxWuilLejuN3CoM/scene.splinecode"
                     className="w-full h-full object-cover"
